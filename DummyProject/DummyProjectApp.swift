93d4aacac0f0aa3085509300f0e927bb9cfbc77a
abhishekyadaviOS <webdevoliva@gmail.com>
2024-09-03 18:37:46 +0530

    Initial Commit

---
+++ DummyProject/DummyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyProjectApp.swift
+//  DummyProject
+//
+//  Created by Abhishek Yadav on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DummyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
